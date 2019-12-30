6b54f16ea08141793dabf7456f1c8e69ab8d3088
Rusab Asher <Rusab.A.Asher@aexp.com>
2019-12-30 20:36:34 +0000

    Almost perfect grid

--- app/src/main/java/com/kyoudai/sudioku/MainActivity.java
+++ app/src/main/java/com/kyoudai/sudioku/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         drawableCellArray = new Drawable[9][9];
+        //Set rights which are column divisions
         drawableCellArray[0][2] = getResources().getDrawable(R.drawable.cellright);
         drawableCellArray[0][5] = getResources().getDrawable(R.drawable.cellright);
         drawableCellArray[0][8] = getResources().getDrawable(R.drawable.cellright);
@@ -59,6 +60,36 @@ public class MainActivity extends AppCompatActivity {
         drawableCellArray[8][2] = getResources().getDrawable(R.drawable.cellbottomright);
         drawableCellArray[8][5] = getResources().getDrawable(R.drawable.cellbottomright);
         drawableCellArray[8][8] = getResources().getDrawable(R.drawable.cellbottomright);
+
+        //Set Bottoms which are row divisions
+        drawableCellArray[2][0] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[2][1] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[2][3] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[2][4] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[2][6] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[2][7] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[5][0] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[5][1] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[5][3] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[5][4] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[5][6] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[5][7] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[8][0] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[8][1] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[8][3] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[8][4] = getResources().getDrawable(R.drawable.cellbottom);
+
+        drawableCellArray[8][6] = getResources().getDrawable(R.drawable.cellbottom);
+        drawableCellArray[8][7] = getResources().getDrawable(R.drawable.cellbottom);
+
+
 
 
 
