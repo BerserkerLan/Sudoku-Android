7787483743ad666971efd6a49de80e614a62e376
Rusab Asher <Rusab.A.Asher@aexp.com>
2019-12-30 20:43:05 +0000

    refactored

--- app/src/main/java/com/kyoudai/sudioku/MainActivity.java
+++ app/src/main/java/com/kyoudai/sudioku/MainActivity.java
@@ -22,7 +22,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sudokuGrid = findViewById(R.id.sudokuGrid);
 
+        setupDrawableForGrid();
+        setGrid();
+        findViewById(R.id.refreshButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setGrid();
+            }
+        });
+
+    }
+
+    public void setGrid() {
+        int dimension = 9;
+        int numberOfDigitsToRemove = 50;
+
+        Sudoku sudoku = new Sudoku(dimension, numberOfDigitsToRemove);
+        sudoku.fillValues();
+        sudoku.printSudoku();
+
+        int[][] matrix = sudoku.returnMatrix();
+
+        TextView numberCell;
+        sudokuGrid.removeAllViews();
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(110, 110);
+
+        for (int i = 0; i < matrix.length; i++) {
+            int[] ints = matrix[i];
+            for (int i1 = 0; i1 < ints.length; i1++) {
+                int anInt = ints[i1];
+                numberCell = new TextView(getApplicationContext());
+                if (anInt != 0) {
+                    numberCell.setText("" + anInt);
+                }
+                numberCell.setLayoutParams(params);
+                if (drawableCellArray[i][i1] != null) {
+                    numberCell.setBackground(drawableCellArray[i][i1]);
+                }
+                else {
+                    numberCell.setBackground(getResources().getDrawable(R.drawable.rectback));
+                }
+                numberCell.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                numberCell.setTextSize(33);
+//                numberCell.setBackgroundResource(R.drawable.cellbottom);
+                sudokuGrid.addView(numberCell);
+            }
+        }
+    }
+
+    public void setupDrawableForGrid() {
         drawableCellArray = new Drawable[9][9];
         //Set rights which are column divisions
         drawableCellArray[0][2] = getResources().getDrawable(R.drawable.cellright);
@@ -89,59 +139,6 @@ public class MainActivity extends AppCompatActivity {
         drawableCellArray[8][6] = getResources().getDrawable(R.drawable.cellbottom);
         drawableCellArray[8][7] = getResources().getDrawable(R.drawable.cellbottom);
 
-
-
-
-
-
-
-        sudokuGrid = findViewById(R.id.sudokuGrid);
-       setGrid();
-
-        findViewById(R.id.refreshButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setGrid();
-            }
-        });
-
-    }
-
-    public void setGrid() {
-        int dimension = 9;
-        int numberOfDigitsToRemove = 50;
-
-        Sudoku sudoku = new Sudoku(dimension, numberOfDigitsToRemove);
-        sudoku.fillValues();
-        sudoku.printSudoku();
-
-        int[][] matrix = sudoku.returnMatrix();
-
-        TextView numberCell;
-        sudokuGrid.removeAllViews();
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(110, 110);
-
-        for (int i = 0; i < matrix.length; i++) {
-            int[] ints = matrix[i];
-            for (int i1 = 0; i1 < ints.length; i1++) {
-                int anInt = ints[i1];
-                numberCell = new TextView(getApplicationContext());
-                if (anInt != 0) {
-                    numberCell.setText("" + anInt);
-                }
-                numberCell.setLayoutParams(params);
-                if (drawableCellArray[i][i1] != null) {
-                    numberCell.setBackground(drawableCellArray[i][i1]);
-                }
-                else {
-                    numberCell.setBackground(getResources().getDrawable(R.drawable.rectback));
-                }
-                numberCell.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                numberCell.setTextSize(33);
-//                numberCell.setBackgroundResource(R.drawable.cellbottom);
-                sudokuGrid.addView(numberCell);
-            }
-        }
     }
 
 }
