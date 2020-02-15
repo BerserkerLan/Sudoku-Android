7d73b7532c76304e1b977ac527c2172779af4e73
BerserkerLan <rusab3529>
2020-02-15 22:37:17 +0000

    Tutorial Popup done

--- app/src/main/java/com/kyoudai/sudioku/TutorialPopup.java
+++ app/src/main/java/com/kyoudai/sudioku/TutorialPopup.java
@@ -3,12 +3,23 @@ package com.kyoudai.sudioku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.synnapps.carouselview.CarouselView;
 
 public class TutorialPopup extends AppCompatActivity {
 
+    CarouselView carouselView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_popup);
+
+        findViewById(R.id.backToHomeFromTutButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
